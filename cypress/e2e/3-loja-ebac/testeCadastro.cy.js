448ea44a0a0e7f5302372fe499622e4a601c31e5
Hellen Taís <taistj3@gmail.com>
2024-06-24 12:53:24 -0400

    teste cadastro - faker e variaveis

---
+++ cypress/e2e/3-loja-ebac/testeCadastro.cy.js
@@ -0,0 +1,42 @@
+///<reference types="cypress"/>
+
+import { faker } from '@faker-js/faker'
+
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+  })
+
+  describe('Funcionalidade: cadastro', () => {
+
+    it('Deve completar o cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('teste12345')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName())
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('exist')
+
+    });
+
+    it.only('Deve completar o cadastro com sucesso - usando variáveis', () => {
+       
+        var nome = faker.person.firstName()
+        var email = faker.internet.email(nome)
+        var sobrenome = faker.person.lastName()
+        
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type('teste12345')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nome)
+        cy.get('#account_last_name').type(sobrenome)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('exist')
+
+    });
+    
+  });
