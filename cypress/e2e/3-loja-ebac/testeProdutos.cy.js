d37bc491dc364bde13eec68a31e0686b9f22c27f
Hellen Taís <taistj3@gmail.com>
2024-06-24 14:31:29 -0400

    teste lista de produtos modificado

--- cypress/e2e/3-loja-ebac/testeProdutos.cy.js
+++ cypress/e2e/3-loja-ebac/testeProdutos.cy.js
@@ -1,20 +1,30 @@
 ///<reference types="cypress"/>
 
+
 beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/produtos/#')
-  })
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+});
 
-  describe('Funcionalidade: produtos', () => {
+  describe('Funcionalidade - lista de produtos', () => {
 
-    it('Deve selecionar um produto da lista', () => {
-       cy.get(' .product-block ').first().click()
-       cy.visit('http://lojaebac.ebaconline.art.br/produtos/#')
-       cy.get(' .product-block ').last().click()
-       cy.visit('http://lojaebac.ebaconline.art.br/produtos/#')
-       cy.get(' .product-block ').eq(1).click()
-       cy.visit('http://lojaebac.ebaconline.art.br/produtos/#')
-       cy.get(' .product-block ').contains('Apollo Running Short').click()
-       cy.get('#tab-title-description > a').should('exist')
-
+    it('Deve selecionar o primeiro produto da lista', () => {
+        cy.get(' .product-block ').first().click()
+        cy.get('#tab-title-description > a').should('exist')
     });
-  });
+
+    it('Deve selecionar o último produto da lista', () => {
+        cy.get(' .product-block ').last().click()
+        cy.get('#tab-title-description > a').should('exist')
+     });
+
+     it('Deve selecionar o segundo produto da lista', () => {
+        cy.get(' .product-block ').eq(1).click()
+        cy.get('#tab-title-description > a').should('exist')
+     });
+
+     it('Deve selecionar o quinto produto da lista', () => {
+        cy.get(' .product-block ').contains('Apollo Running Short').click()
+        cy.get('#tab-title-description > a').should('exist')
+     });
+        
+    });
